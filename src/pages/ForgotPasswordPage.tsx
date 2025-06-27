b303d5ed014ea003064ec4fa39feb73038834693
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-28 02:41:42 +0530

    Updated code files via regeneration: ForgotPasswordPage.tsx

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -49,7 +49,7 @@ const ForgotPasswordPage = () => {
   }
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-1 flex items-center justify-center p-4">
         <AuthFormWrapper
